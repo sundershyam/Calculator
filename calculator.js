abe7a86a89627b7bb712f6af0c9b9d1b0c29d312
sundershyam <shamsundertejpal@gmail.com>
2021-07-12 18:17:05 +0530

    commit

--- calculator.js
+++ calculator.js
@@ -30,36 +30,18 @@ for(i=0;i<clic.length;i++){
 
 
 function selectoperator(classname,inputid){
-   var clic = document.getElementByClassName(classname);
-   alert(clic);
-for(i=0;i<clic.length;i++){
-   clic[i].addEventListener("click",function(){
-      var index = this.innerText;
-      // var input =document.getElementById(inputid);
-      // var input2 =document.getElementById("cal-screen-display-input2");
-      // if(input.innerHTML.length<=1){
-      //    input.innerHTML=index;
-      // }   
-      alert(document.getElementById("cal-screen-display-input1").innerText); 
-   });
-}
-}
+   var Element = document.getElementByClassName(classname);
 
+}
 
 function makeeqa(){
    // keypress("cal-pannel-keyboard-pannel-buttons","cal-screen-display-input1");
    // keypress("cal-pannel-keyboard-pannel-buttons","cal-screen-display-input2");
    // selectoperator("c-p-k-o-b-operator","cal-screen-display-operator");
-   var input1 = document.getElementById("cal-screen-display-input1");
-   var input2 = document.getElementById("cal-screen-display-input2");
-   var operator = document.getElementById("cal-screen-display-operator");
-   if(operator.innerText.length==0){
-      keypress("cal-pannel-keyboard-pannel-buttons","cal-screen-display-input1");
-   } 
-  if(input1.innerText.length>0){
-   selectoperator("c-p-k-o-b-operator","cal-screen-display-operator");
-   alert("Functioncalled")
-      }
+   // var input1 = document.getElementById("cal-screen-display-input1");
+   // var input2 = document.getElementById("cal-screen-display-input2");
+   // var operator = document.getElementById("cal-screen-display-operator");
+   
    
 }
 
